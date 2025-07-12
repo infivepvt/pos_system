e476917eaa9e4ce1dd4c5f0d0d8ab7ce457c1f61
infivepvt <infivellc@gmail.com>
2025-07-12 13:05:36 +0530

    ok

--- config.php
+++ config.php
@@ -1,29 +1,18 @@
 <?php
-date_default_timezone_set('Asia/Colombo');
-
 $host = 'localhost';
-$port = '3307'; // <-- specify your port here
-$db   = 'pos_system';
-$user = 'root';
-$pass = 'infive1234';
+$db = 'u263749830_simplepos';
+$user = 'u263749830_pos';
+$pass = '+hE27|SGvA0';
 
 try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
 
 // Helper function to generate invoice number
-function generateInvoiceNumber()
-{
+function generateInvoiceNumber() {
     return 'INV-' . time();
 }
-
-
-// Mailer configurations
-$mail_host = 'smtp.gmail.com'; // Set your SMTP server
-$mail_username = 'nethminasachindu7@gmail.com'; // SMTP username
-$mail_password = 'lpdiXqbpIoujPvhh'; // SMTP password
-$mail_admin_email = 'sachindunethmina6@gmail.com'; // Admin email address
-$mail_from_name = 'Infive POS System'; // From name
+?>
